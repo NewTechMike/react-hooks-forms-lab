6fc5e38282f3bd0a1bcdf90a7a0a949f17b83904
Michael Harris <xero5k@yahoo.com>
2021-12-03 09:03:53 -0600

    filter and itemForm

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Filter({ onCategoryChange }) {
+
+function Filter({ onCategoryChange, onSearchChange }) {
+
+  const [search, setSearch] = useState("")
+  
+  function onSearchChange(event){
+    if (event.target.value === "value"){
+      console.log("Checking Value")
+    }
+    setSearch(event.target.value)
+  } 
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
-      <select name="filter" onChange={onCategoryChange}>
+      <input 
+        type="text" 
+        name="search" 
+        onChange={onSearchChange} 
+        value = {search} 
+        placeholder="Search..."
+      />
+
+      <select name="filter" search={search} onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,24 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({itemName, itemCategory, onItemDataChange}) {
+
+  const [array, setArray] = useState({
+    id: uuid(),
+    name: itemName,
+    category: itemCategory,
+  })
+
+  function onItemFormSubmit(event){
+    event.preventDefault();
+    const name = event.target.name.value;
+    const category = event.target.category;
+    console.log(name, category, event.target)
+    setArray({
+      ...array, 
+      [name]: category,
+    });
+    console.log("array: ", array)
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={onItemFormSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={array.name}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" value={array.category}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </label>
 
-      <button type="submit">Add to List</button>
+      <button type="submit" >Add to List</button>
     </form>
   );
 }
